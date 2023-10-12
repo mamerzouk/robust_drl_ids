7d2fed7f096fb8843e525f61554431bc1c56012f
Mohamed Amine Merzouk <mamerzouk@protonmail.com>
2023-10-12 16:59:11 -0400

    Remove 0.00 from the epsilon_range

--- scripts/train_multiple_agents.py
+++ scripts/train_multiple_agents.py
@@ -46,7 +46,7 @@ if __name__=='__main__':
     nb_agents = args.nb_agents
     binary = args.binary
     verbose = False
-    epsilon_range=[0., 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+    epsilon_range=[0., 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
 
     # Workspace config parameters
     device_name = 'cpu' 
